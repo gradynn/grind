7c12d3d0ae67b8cf3921734f9421a66465b07f86
Gradyn Nagle <gradyndnagle@outlook.com>
2024-04-19 18:54:28 -0700

    some basic frontend skeleton files

--- grind-frontend/src/screens/home.tsx
+++ grind-frontend/src/screens/home.tsx
@@ -1,7 +1,15 @@
+import TaskList from "@src/components/TaskList";
+
 const Home = () => {
     return (
-        <div>
-        <h1>Home</h1>
+        <div className="h-screen">
+            <div className="bg-indigo-500 h-[5vh]">menu</div>
+            <div className="flex flex-row flex-grow">
+                <div className="w-[30%] bg-blue-500">
+                    <TaskList />
+                </div>
+                <div className="w-[70%] bg-green-500">calendar</div>
+            </div>
         </div>
     )
 }

9242f17b5e50c5401274faf1be72bc75e676bfee
Gradyn Nagle <gradyndnagle@outlook.com>
2024-04-23 09:43:59 -0700

    commit from root, small changes

--- grind-frontend/src/components/TaskList/index.tsx
+++ grind-frontend/src/components/TaskList/index.tsx
@@ -1,4 +1,9 @@
+import { UseState } from 'react';
+import Task from '@src/models/task';
+
 const TaskList = () => {
+   const [activeTasks, setActiveTasks] = useState<Task[]>([]); 
+    
     return (
         <div>
         <h1>Task List</h1>
---
+++ grind-frontend/src/models/task.ts
@@ -0,0 +1,8 @@
+interface Task {
+    name: string;
+    description: string;
+    completed: boolean;
+    deadline: Date;
+}
+
+export default Task;

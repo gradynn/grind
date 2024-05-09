9cf37a6c21210b07bc0876c27191700f9b69c683
Gradyn Nagle <gradyndnagle@outlook.com>
2024-05-09 10:22:57 -0700

    added task schema for MongoDB interactions

---
+++ grind-backend/src/models/Task.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const taskSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    dueDate: { type: Date, required: false },
+    type: { type: String, enum: ['STORY', 'TASK'], default: 'STORY', required: true },
+    points: { type: Number, required: false },
+    status: { type: String, enum: ['TODO', 'IN_PROGRESS', 'DONE'], default: 'TODO', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+const Task = model('Task', taskSchema);
+export default Task;
